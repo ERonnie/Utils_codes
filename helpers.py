7b5595b5d3cc2e84b50fd96df63981515495bb54
Ronaldo Esteves Ianelli <ronnyz101112@gmail.com>
2025-09-30 10:55:03 -0300

    add carregar_arquivo

--- helpers.py
+++ helpers.py
@@ -29,3 +29,41 @@ def salvar_csv(df: pd.DataFrame,
         print(f"Arquivo salvo em {arquivo}\nArquivo {nome_arquivo} salvo em {fim - inicio:.2f} segundos.")
     except Exception as e:
         print(f"Erro ao salvar o arquivo {nome_arquivo}: {e}")
+
+def carregar_arquivo(caminho: str, **kwargs) -> pd.DataFrame:
+    print("Iniciando o carregamento do arquivo")
+    extensao = os.path.splitext(caminho)[1].lower()
+    # parâmetros padrão
+    defaults = {"csv": {"sep": ";", "decimal": ",", "encoding": "utf-8"},
+                "excel": {"engine": None, "decimal": ",", "thousands": "."},
+                "xlsb": {"engine": "pyxlsb", "decimal": ",", "thousands": "."}
+                }
+    
+    inicio = time.time()
+
+    match extensao:
+        case ".csv":
+            print("Extensão .csv detectada.")
+            params = {**defaults["csv"], **kwargs}
+            try:
+                print("Lendo o arquivo csv em UTF-8")
+                df = pd.read_csv(caminho, **params)
+            except UnicodeDecodeError:
+                print("Não foi possível ler em UTF-8, tentando latin1")
+                params["encoding"] = "latin1"
+                df = pd.read_csv(caminho, **params) 
+        case ".xlsx" | ".xls" | ".xlsm":
+            print("Extensão excel detectada.")
+            params = {**defaults["excel"], **kwargs}
+            df = pd.read_excel(caminho, **params)
+        case ".xlsb":
+            print("Extensâo .xlsb detectada.")
+            params = {**defaults["xlsb"], **kwargs}
+            df = pd.read_excel(caminho, **params)
+        case _:
+            raise ValueError(f"Extensão de arquivo '{extensao}' não suportada.")
+    
+    fim = time.time()
+
+    print(f"Arquivo {os.path.basename(caminho)} carregado em {fim - inicio:.2f} segundos.")
+    return df
